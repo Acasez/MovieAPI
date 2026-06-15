9f7ec24d407f4f54eeae812114a988c4dc981f03
Acasez <edvin.sanne@gmail.com>
2026-06-15 14:17:45 +0200

    Updated vurnable packages

--- MovieAPI/DataTrransferObjects/MovieCreateDTO.cs
+++ MovieAPI/DataTrransferObjects/MovieCreateDTO.cs
@@ -2,5 +2,9 @@
 
 public class MovieCreateDTO
 {
-
+    public string Name { get; set; } = string.Empty;
+    public string? Title { get; set; }
+    public int? Year { get; set; }
+    public string? Genre { get; set; }
+    public int? Duration { get; set; }
 }
