bc38704486918346ae6129754b2bd71c981bea76
Acasez <edvin.sanne@gmail.com>
2026-06-15 14:48:16 +0200

    Setup first mapper

---
+++ MovieAPI/Profiles/MovieProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace MovieAPI.Profiles;
+
+public class MovieProfiles : Profile
+{
+    public MovieProfiles()
+    {
+        CreateMap<DataTrransferObjects.MovieCreateDTO, Models.Movie>();
+        CreateMap<Models.Movie, DataTrransferObjects.MovieCreateDTO>();
+    }
+}
