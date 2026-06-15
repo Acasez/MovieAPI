c654ac640ae0afe7414a679d23386144bf47e785
Acasez <edvin.sanne@gmail.com>
2026-06-15 13:59:33 +0200

    Created model classes

---
+++ MovieAPI/Models/Actor.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieAPI.Models;
+
+public class Actor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+}
---
+++ MovieAPI/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieAPI.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string ReviewerName { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+    public int MovieId { get; set; }
+}
