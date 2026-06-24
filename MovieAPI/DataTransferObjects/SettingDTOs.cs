8745327a7d21d80245e00ede537c98909263cbbd
Acasez <edvin.sanne@gmail.com>
2026-06-24 09:14:21 +0200

    Set up settings DTO

---
+++ MovieAPI/DataTransferObjects/SettingDTOs.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.DataTransferObjects;
+// ReSharper disable once InconsistentNaming
+public class SettingDTO
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public  ICollection<MovieDTO> Movies { get; set; } = [];
+}
+// ReSharper disable once InconsistentNaming
+public class SettingCreateDTO
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public  ICollection<MovieDTO> Movies { get; set; } = [];
+}
+// ReSharper disable once InconsistentNaming
+public class SettingUpdateDTO
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; }
+}
