91924b44723c3024b261e40d4fd0c296b758dff0
Acasez <edvin.sanne@gmail.com>
2026-06-26 14:14:41 +0200

    And can unit test for GetMovie too

--- MovieAPI.Tests/UnitTest1.cs
+++ MovieAPI.Tests/UnitTest1.cs
@@ -88,122 +88,53 @@ namespace MovieAPI.Tests
             Assert.Equal("Test Movie 2", returnedMovies[1].Title);
         }
 
-        // [Fact]
-        // public async Task GetMovieById_ReturnsOkWithMovie()
-        // {
-        //
-        //     // Arrange
-        //     MovieDTO movieDto = new MovieDTO
-        //     {
-        //         Id = 1,
-        //         Title = "Test Movie",
-        //         Year = 2000,
-        //         Duration = 120,
-        //         GenreId = 1,
-        //     };
-        //
-        //     Mock<MoviesController> mockService = new();
-        //
-        //     mockService.Setup(s => s.GetMovie(1)).ReturnsAsync(movieDto);
-        //
-        //     MoviesController controller = new(mockService.Object);
-        //
-        //     // Act
-        //     ActionResult<MovieDto> result = await controller.GetMovieById(1);
-        //
-        //     // Assert
-        //     OkObjectResult okResult = Assert.IsType<OkObjectResult>(result.Result);
-        //
-        //     MovieDto returnedMovie = Assert.IsType<MovieDto>(okResult.Value);
-        //
-        //     Assert.Equal(1, returnedMovie.Id);
-        //     Assert.Equal("Test Movie", returnedMovie.Title);
-        // }
-        //
-        // [Fact]
-        // public async Task GetMovieDetails_ReturnsOK()
-        // {
-        //     // Arrange
-        //     var movieDto = new MovieDto {
-        //         Id = 1,
-        //         Title = "Test Movie",
-        //         Year = null,
-        //         Duration = null,
-        //         Details = new MovieDetailsDto {
-        //             Synopsis = "SynopsisTest.",
-        //             Language = "Engelska",
-        //             Budget = "$Test"
-        //         },
-        //         Actors = new List<ActorDto>(),
-        //         Genres = new List<GenreDto>(),
-        //         Reviews = new List<ReviewDto>()
-        //     };
-        //
-        //     var mockService = new Mock<IMovieService>();
-        //
-        //     mockService
-        //         .Setup(s => s.GetMovieDetails(1))
-        //         .ReturnsAsync(movieDto);
-        //
-        //     var controller = new MoviesController(mockService.Object);
-        //
-        //     // Act
-        //     var result = await controller.GetMovieDetails(1);
-        //
-        //     // Assert
-        //     var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        //     var returnedMovie = Assert.IsType<MovieDetailsDto>(okResult.Value);
-        //     Assert.Equal("Test Movie", returnedMovie.Title);
-        //
-        //     var returnedDetails = Assert.IsType<MovieDetailsDto>(okResult.Value);
-        //     Assert.Equal("SynopsisTest.", returnedDetails.Synopsis);
-        //     Assert.Equal("Engelska", returnedDetails.Language);
-        //     Assert.Equal("$Test", returnedDetails.Budget);
-        // }
-        //
-        // [Fact]
-        // public async Task GetMovieReviewByMovieId_ReturnsOKWithReviews()
-        // {
-        //     // Arrange
-        //     var movieDto = new MovieDto {
-        //         Id = 1,
-        //         Title = "Test Movie",
-        //         Year = null,
-        //         Duration = null,
-        //         Details = new MovieDetailsDto(),
-        //         Actors = new List<ActorDto>(),
-        //         Genres = new List<GenreDto>(),
-        //         Reviews = new List<ReviewDto> {
-        //             new ReviewDto {
-        //                 ReviewId = 1,
-        //                 ReviewerName = "Test Reviewer",
-        //                 Rating = 5,
-        //                 Comment = "Great movie!"
-        //             }
-        //         }
-        //     };
-        //
-        //     var mockService = new Mock<IMovieService>();
-        //
-        //     mockService
-        //         .Setup(s => s.GetMovieReviews(1))
-        //         .ReturnsAsync(movieDto);
-        //
-        //     var controller = new MoviesController(mockService.Object);
-        //
-        //     // Act
-        //     var result = await controller.GetMovieReviews(1);
-        //
-        //     // Assert
-        //     var okResult = Assert.IsType<OkObjectResult>(result.Result);
-        //     var reviews = Assert.IsType<List<ReviewDto>>(okResult.Value);
-        //
-        //     var returnedReviews = Assert.IsType<List<ReviewDto>>(reviews);
-        //     Assert.Single(returnedReviews);
-        //     //Assert.Equal(1, returnedReviews[0].Id);
-        //     Assert.Equal("Test Reviewer", returnedReviews[0].ReviewerName);
-        //     Assert.Equal(5, returnedReviews[0].Rating);
-        //     Assert.Equal("Great movie!", returnedReviews[0].Comment);
-        // }
+        [Fact]
+        public async Task GetMovieById_ReturnsOkWithMovie()
+        {
+
+            Movie exampleMovie = new()
+            {
+                Id = 1,
+                Title = "Test Movie",
+                Year = 2000,
+                Duration = 120,
+                GenreId = 1,
+            };
+        
+            // Arrange
+            MovieDTO movieDto = new()
+            {
+                Id = 1,
+                Title = "Test Movie",
+                Year = 2000,
+                Duration = 120,
+                GenreId = 1,
+            };
+        
+            // Mock the repository with parameters
+            Mock<IMovieService> mockService = new();
+            mockService
+                .Setup(s => s.GetMovieAsync(0)).ReturnsAsync(exampleMovie);
+            
+            // Mock the mapper
+            Mock<IMapper> mockMapper = new();
+            mockMapper
+                .Setup(m => m.Map<MovieDTO>(It.IsAny<Movie>()))
+                .Returns(movieDto); // Return as array to match the controller
+
+        
+            MoviesController controller = new(mockService.Object, mockMapper.Object);
+        
+            // Act
+            ActionResult<MovieDTO> result = await controller.GetMovie(0);
+        
+            // Assert
+            OkObjectResult okResult = Assert.IsType<OkObjectResult>(result.Result);
+        
+            MovieDTO returnedMovie = Assert.IsType<MovieDTO>(okResult.Value);
+        
+            Assert.Equal(1, returnedMovie.Id);
+            Assert.Equal("Test Movie", returnedMovie.Title);
+        }
     }
 }
